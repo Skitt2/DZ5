748ed6a0248fac7d8cac0567a4db60c8b71d98d9
Skitt2 <provaid@yandex.ru>
2021-12-08 11:40:52 +0300

    Добавлена 6я задача

--- DZ5.6/Program.cs
+++ DZ5.6/Program.cs
@@ -10,18 +10,78 @@ namespace DZ5._6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Запросить у пользователя целочисленное значение N. Сформировать двумерный массив размера NxN. Заполнить массив числами, вводимыми с клавиатуры. Проверить, является ли введенная с клавиатуры матрица магическим квадратом. Магическим квадратом называется матрица, сумма элементов которой в каждой строке, в каждом столбце и по каждой диагонали одинакова.");
-            Console.Write("целочисленное значение N = ");
+            Console.WriteLine("Запросить у пользователя целочисленное значение N. Сформировать двумерный массив размера NxN следующего вида:");
+            Console.WriteLine("Введите размер матрицы");
             int n = Convert.ToInt32(Console.ReadLine());
-            int[,] t = new int[n, n];
+            int[,] mas = new int[n, n];
+            int sumD1 = 0;
+            int sumD2 = 0;
+            int sumG1 = 0;
+            int sumG2 = 0;
+            int sumV1 = 0;
+            int sumV2 = 0;
+            bool f = false;
+            Console.WriteLine("Заполни матрицу");
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
-                    t[i, j] = Convert.ToInt32(Console.Read());
-                    //Console.Write("{0,4}", t[i, j]);
+                    mas[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("{0} ", mas[i, j]);
                 }
                 Console.WriteLine();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (j == i)
+                    {
+                        sumD1 += mas[i, j];
+                    }
+                    if (j == n - (i + 1))
+                    {
+                        sumD2 += mas[i, j];
+                    }
+                    sumG1 += mas[i, j];
+
+                    if (sumG2 != sumG1 && j == n - 1 && i > 0)
+                    {
+                        f = true;
+                        break;
+                    }
+                }
+                sumG2 = sumG1;
+                sumG1 = 0;
+            }
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    sumV1 += mas[i, j];
+
+                    if (sumV2 != sumV1 && i == n - 1 && j > 0)
+                    {
+                        f = true;
+                        break;
+                    }
+                }
+                sumV2 = sumV1;
+                sumV1 = 0;
+            }
+            if (sumD1 == sumD2 && sumG2 == sumV2 && sumD2 == sumG2 && f == false)
+            {
+                Console.WriteLine("матрица является магическим квадратом");
+            }
+            else
+            {
+                Console.WriteLine("матрица не является магическим квадратом");
             }
             Console.ReadKey();
         }
